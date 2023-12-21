67058389f87d5c4637e424b3580e3e9853a3c69b
NKryzhuk <88149030+NKryzhuk@users.noreply.github.com>
2023-12-21 12:01:19 +0200

    Add files via upload

---
+++ task2.erl
@@ -0,0 +1,13 @@
+-module(task2).
+
+-export([but_last/1]).
+
+but_last(List)->
+    Len =  task4:len(List),
+    [_|T] = List,
+    if
+        Len =< 2 -> 
+            List;
+        true ->
+            but_last(T)
+    end.
