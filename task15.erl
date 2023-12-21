d1bd9accde7b352f495764caf36fbd19c538b92d
NKryzhuk <88149030+NKryzhuk@users.noreply.github.com>
2023-12-21 12:08:50 +0200

    Add files via upload

---
+++ task15.erl
@@ -0,0 +1,15 @@
+-module(task15).
+
+-export([replicate/2,replicate/3]).
+
+replicate(List, K) -> replicate(List, K, K).
+
+replicate([], _, _) -> [];
+replicate([H|T], K, I) ->
+    if 
+        I > 1 ->
+            [H|replicate([H|T],K,I-1)];
+        true->
+            [H|replicate(T,K,K)]
+        end.
+
